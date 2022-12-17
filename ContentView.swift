79a8974580755210a36996ffca31e0a5f8dc4c1b
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-17 14:24:16 -0600

    Create ContentView.swift

---
+++ ContentView.swift
@@ -0,0 +1,235 @@
+//
+//  ContentView.swift
+//  AppRate
+//
+//  Created by Jayce Sagvold on 12/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        
+        ScrollView {
+            
+            
+            VStack {
+                
+                
+                Image("Twitter")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .cornerRadius(12.5)
+                    .padding()
+                
+                
+                Text("Twitter")
+                    .font(.title2)
+                    .bold()
+                    .padding(.bottom, 2.5)
+                
+                
+                Text("Rating:")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 6)
+                
+                Text("Overall 4.77/5")
+                    .padding(.bottom, 2.5)
+                    .foregroundColor(.green)
+                    .bold()
+                
+                Text("Community: 4.2/5")
+                    .padding(.bottom, 2.5)
+                
+                Text("App: 4.5/5")
+                    .padding(.bottom, 2.5)
+                
+                Text("Features: 4.25/5")
+                    .padding(.bottom, 50)
+                
+                
+            }
+            
+            
+            VStack {
+                
+                
+                Image("Twitch")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .cornerRadius(12.5)
+                    .padding()
+                
+                
+                Text("Twitch")
+                    .font(.title2)
+                    .bold()
+                    .padding(.bottom, 2.5)
+                
+                
+                Text("Rating:")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 6)
+                
+                Text("Overall 2.3/5")
+                    .padding(.bottom, 2.5)
+                    .foregroundColor(.red)
+                    .bold()
+                
+                Text("Community: 3.9/5")
+                    .padding(.bottom, 2.5)
+                
+                Text("App: 3.05/5")
+                    .padding(.bottom, 2.5)
+                
+                Text("Quality: 2/5")
+                    .padding(.bottom, 2.5)
+                
+                Text("Features: 2/5")
+                    .padding(.bottom, 50)
+                
+                
+            }
+            
+            VStack {
+                
+                
+                Image("Google")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .cornerRadius(12.5)
+                    .padding()
+                
+                
+                Text("Google")
+                    .font(.title2)
+                    .bold()
+                    .padding(.bottom, 2.5)
+                
+                
+                Text("Rating:")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 6)
+                
+                Text("Overall 4.5/5")
+                    .padding(.bottom, 2.5)
+                    .foregroundColor(.green)
+                    .bold()
+                
+                Text("Community: NA/5")
+                    .padding(.bottom, 2.5)
+                
+                Text("App: 4.25/5")
+                    .padding(.bottom, 2.5)
+                
+                Text("Quality: 4.6/5")
+                    .padding(.bottom, 2.5)
+                
+                Text("Features: 4.88/5")
+                    .padding(.bottom, 50)
+                
+                
+            }
+            
+            
+            VStack {
+                
+                
+                Image("Spotify")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .cornerRadius(12.5)
+                    .padding()
+                
+                
+                Text("Spotify")
+                    .font(.title2)
+                    .bold()
+                    .padding(.bottom, 2.5)
+                
+                
+                Text("Rating:")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 6)
+                
+                Text("Overall 3.4/5")
+                    .padding(.bottom, 2.5)
+                    .foregroundColor(.yellow)
+                    .bold()
+                
+                Text("Community: NA/5")
+                    .padding(.bottom, 2.5)
+                
+                Text("App: 3.6/5")
+                    .padding(.bottom, 2.5)
+                
+                Text("Quality: 3/5")
+                    .padding(.bottom, 2.5)
+                
+                Text("Features: 4.5/5")
+                    .padding(.bottom, 50)
+                
+                
+            }
+            
+            
+            VStack {
+                
+                
+                Image("Pandora")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .cornerRadius(12.5)
+                    .padding()
+                
+                
+                Text("Pandora")
+                    .font(.title2)
+                    .bold()
+                    .padding(.bottom, 2.5)
+                
+                
+                Text("Rating:")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 6)
+                
+                Text("Overall 4.6/5")
+                    .padding(.bottom, 2.5)
+                    .foregroundColor(.yellow)
+                    .bold()
+                
+                Text("Community: NA/5")
+                    .padding(.bottom, 2.5)
+                
+                Text("App: 4.8/5")
+                    .padding(.bottom, 2.5)
+                
+                Text("Quality: 4.5/5")
+                    .padding(.bottom, 2.5)
+                
+                Text("Features: 4.5/5")
+                    .padding(.bottom, 50)
+                
+                
+            }
+            
+                
+                
+            }
+        }
+        
+        struct ContentView_Previews: PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
+    }
+
+
+// Use this site for the reviews: "https://www.sitejabber.com"
